e1522b3191cb507d4b807cde0e8bdf792c10b686
Julienboyreau <julienboyreau@yahoo.fr>
2020-11-13 14:06:38 +0100

    Delete meta.js

--- display/meta.js
+++
@@ -1,9 +0,0 @@
-module.exports = (domain,produit) => `
-    <!-- Open Graph meta pour Facebook -->
-    <meta property="og:title" content="Jouez à Umanitus ${produit && produit.but ? '- '+produit.but :''}" />
-    <meta property="og:url" content="${ produit && produit.id ? 'https://'+domain+'/'+produit.id:''}" />
-    <meta property="og:image" content="${produit && produit.image ? produit.image : ''}" />
-    <meta property="og:description" content="${produit && produit.description ? produit.description : ''}" />
-    <meta property="og:site_name" content="${domain}" />
-    <meta property="og:type" content="article" />
-`
