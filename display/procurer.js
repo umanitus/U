8b3bd9555643af5008f62f2ee89563feeecf12d8
Julienboyreau <julienboyreau@yahoo.fr>
2020-11-13 14:07:31 +0100

    Delete procurer.js

--- display/procurer.js
+++
@@ -1,7 +0,0 @@
-module.exports = (id) => `
-    <form>
-        <button style="background-color:blue;" hx-post="/procurer" hx-target='closest .actions'>Procurer</button>
-        <input name="o" value="${id}" style="display:none"/>
-    </form>
-`
-
