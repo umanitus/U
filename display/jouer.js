bd22a55fbb247a075c3e3bfbfaa9113e1ef8f69a
Julienboyreau <julienboyreau@yahoo.fr>
2020-11-13 14:06:07 +0100

    Delete jouer.js

--- display/jouer.js
+++
@@ -1,6 +0,0 @@
-module.exports = (id, but) => `
-    <form>
-        <button class="jouer" style="background-color:green;" hx-post="/${ but == 'Vente' ? 'acheter' : 'vendre'}" hx-target='closest .actions'>Jouer</button>
-        <input name="o" value="${id}" style="display:none"/>
-    </form>
-`
