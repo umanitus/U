340381b5f059d6cd64db7edc8332fb7763cf67aa
Julienboyreau <julienboyreau@yahoo.fr>
2020-11-13 14:07:17 +0100

    Delete passer.js

--- display/passer.js
+++
@@ -1,6 +0,0 @@
-module.exports = id => `
-    <form>
-        <button style="background-color:black;" hx-post="/passer" hx-target='closest .card'>Passer</button>
-        <input name="o" value="${id}" style="display:none"/>
-    </form>
-`
