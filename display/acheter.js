22c8fadae3272fb4cc2d7e48586d443a77cffe40
Julienboyreau <julienboyreau@yahoo.fr>
2020-11-13 14:04:48 +0100

    Delete acheter.js

--- display/acheter.js
+++
@@ -1,6 +0,0 @@
-module.exports = id => `
-    <form>
-        <button style="background-color:green;" hx-post="/obtenir" hx-target='closest .actions'>Obtenir</button>
-        <input name="o" value="${id}" style="display:none"/>
-    </form>
-`
